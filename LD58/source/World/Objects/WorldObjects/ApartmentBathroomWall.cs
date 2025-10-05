49e3d2d34af50377739ee7e9e3575518fe1a16da
frameperfection <nafoizret@web.de>
2025-10-05 02:56:28 +0200

    fix apartment wall asset names

--- LD58/source/World/Objects/WorldObjects/ApartmentBathroomWall.cs
+++ LD58/source/World/Objects/WorldObjects/ApartmentBathroomWall.cs
@@ -1,6 +1,6 @@
 namespace LD58.World.Objects.WorldObjects
 {
-    [DefaultInstancer(64, "objects/ApartmentBathroomWall.gmdl", "objects/ApartmentBathroomWall.mat")]
+    [DefaultInstancer(64, "objects/Apartment Bathroom Wall.gmdl", "objects/Apartment Bathroom Wall.mat")]
     public class ApartmentBathroomWall
         : WorldObject
     { }
--- LD58/source/World/Objects/WorldObjects/ApartmentWall.cs
+++ LD58/source/World/Objects/WorldObjects/ApartmentWall.cs
@@ -1,6 +1,6 @@
 namespace LD58.World.Objects.WorldObjects
 {
-    [DefaultInstancer(64, "objects/ApartmentWall.gmdl", "objects/ApartmentWall.mat")]
+    [DefaultInstancer(64, "objects/Apartment Wall.gmdl", "objects/Apartment Wall.mat")]
     public class ApartmentWall
         : WorldObject
     { }
