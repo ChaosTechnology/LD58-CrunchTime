83f65a4540d1b8e3cad9e653a4a04737a1125721
frameperfection <nafoizret@web.de>
2025-10-06 10:55:35 +0200

    add office chairs

---
+++ LD58/source/World/Objects/WorldObjects/OfficeChair.cs
@@ -0,0 +1,7 @@
+namespace LD58.World.Objects.WorldObjects
+{
+    [DefaultInstancer(64, "objects/Office Chair.gmdl", "objects/Office.mat")]
+    public class OfficeChair
+        : WorldObject
+    { }
+}
