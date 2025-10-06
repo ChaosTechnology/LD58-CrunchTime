0fa7493a070a7934f1b2dd9e5ffbc740362b38d2
frameperfection <nafoizret@web.de>
2025-10-06 03:30:22 +0200

    add office assets

---
+++ LD58/source/World/Objects/WorldObjects/FlatscreenHuge.cs
@@ -0,0 +1,9 @@
+namespace LD58.World.Objects.WorldObjects
+{
+    [DefaultInstancer(64, "objects/Flatscreen Huge.gmdl", "objects/Screen.mat")]
+    class FlatscreenHuge
+        : WorldObject
+    {
+        public FlatscreenHuge() : base(4, 1) { }
+    }
+}
---
+++ LD58/source/World/Objects/WorldObjects/Puff.cs
@@ -0,0 +1,9 @@
+namespace LD58.World.Objects.WorldObjects
+{
+    [DefaultInstancer(64, "objects/Puff.gmdl", "objects/Sofa.mat")]
+    public class Puff
+        : WorldObject
+    {
+        public Puff() : base(2, 2) { }
+    }
+}
