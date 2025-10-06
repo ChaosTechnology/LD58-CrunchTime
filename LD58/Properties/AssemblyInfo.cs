57e859964059dbab0581e8bc5eb48c2632cb6aab
InsanityCode <56584762+InsanityCode@users.noreply.github.com>
2025-10-06 22:48:21 +0200

    dub it "Crunch Time"

--- LD58/Properties/AssemblyInfo.cs
+++ LD58/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("LD58")]
-[assembly: AssemblyProduct("LD58")]
+[assembly: AssemblyTitle("LD58 - Crunch Time")]
+[assembly: AssemblyProduct("LD58 - Crunch Time")]
 [assembly: AssemblyCompany("ChaosTechnology")]
 [assembly: AssemblyCopyright("Copyright © ChaosTechnology 2025")]
 
