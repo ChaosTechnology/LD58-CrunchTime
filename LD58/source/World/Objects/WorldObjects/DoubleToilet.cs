8b9f7f15ccbc400afa55a37c3fae6e40309a3719
frameperfection <nafoizret@web.de>
2025-10-06 23:52:02 +0200

    fix double toilet

--- LD58/source/World/Objects/WorldObjects/DoubleToilet.cs
+++ LD58/source/World/Objects/WorldObjects/DoubleToilet.cs
@@ -1,6 +1,9 @@
+using ChaosFramework.Collections;
 using ChaosFramework.Graphics.OpenGl.Instancing;
 using ChaosFramework.Math;
 using ChaosFramework.Math.Vectors;
+using static ChaosFramework.Math.Clamping;
+using SysCol = System.Collections.Generic;
 
 namespace LD58.World.Objects.WorldObjects
 {
@@ -11,15 +14,28 @@ namespace LD58.World.Objects.WorldObjects
     class DoubleToilet
         : Toilet
     {
+        static string[] lines = new[]
+        {
+            "Uhm... this seems to be a design flaw.",
+            "They really didn't think this through...",
+            "The architects who built this must be as competent as my team...",
+            "Too bad that none of this has any meaning yet.",
+            "...",
+        };
+
+        class UserInteractionState
+        {
+            public int numTimesInteracted = 0;
+        }
+
+        SysCol.Dictionary<Interactor, UserInteractionState> interactors = new SysCol.Dictionary<Interactor, UserInteractionState>();
+
         public override bool Interact(Interactor interactor, Vector2i interactAt)
         {
-            interactor.AddInteraction(
-                new DialogLine(interactor, "What the fuck!"),
-                new Choice(interactor, "Try it out?",
-                    new Choice.Option("How???", null),
-                    new Choice.Option("...", new[] { new Suicide(interactor) })
-                    )
-                );
+            UserInteractionState state = interactors.GetOrCreateValue(interactor);
+            interactor.AddInteraction(new DialogLine(interactor, lines[state.numTimesInteracted]));
+
+            state.numTimesInteracted = Min(lines.Length - 1, state.numTimesInteracted + 1);
             return true;
         }
 
