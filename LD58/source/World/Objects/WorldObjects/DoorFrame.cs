515ae34645c5514d195e98d5caf05360edaa019a
frameperfection <nafoizret@web.de>
2025-10-06 16:17:38 +0200

    enable door frame interactions

--- LD58/source/World/Objects/WorldObjects/DoorFrame.cs
+++ LD58/source/World/Objects/WorldObjects/DoorFrame.cs
@@ -1,9 +1,22 @@
+using System.Collections.Generic;
+using ChaosFramework.Math.Vectors;
+using System.Linq;
+
 namespace LD58.World.Objects.WorldObjects
 {
     [DefaultInstancer(64, "objects/Door Frame.gmdl", "objects/Kitchen.mat")]
     class DoorFrame
         : WorldObject
     {
-        public DoorFrame() : base(0, 0) { }
+        static readonly Vector2i[] DOOR_MAT_POSITIONS = new[] { new Vector2i(0, -1), new Vector2i(1, -1) };
+        static readonly Vector2i[] FRAME_POSITIONS = new[] { new Vector2i(0, 0), new Vector2i(1, 0) };
+
+        bool locked = false;
+
+        protected override IEnumerable<Vector2i> RelativeOffsetsForOccupiedTiles()
+            => DOOR_MAT_POSITIONS.Concat(FRAME_POSITIONS);
+
+        public override bool CanStepOn(Vector2i pos)
+            => !locked || TransformRelativeTilePositions(DOOR_MAT_POSITIONS).Any(x => x == pos);
     }
 }
