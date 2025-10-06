9a339361e37ca0f754cd5a467d8faba5c3620c2e
frameperfection <nafoizret@web.de>
2025-10-06 10:32:07 +0200

    add potted plant

---
+++ LD58/source/World/Objects/WorldObjects/PottedPlant.cs
@@ -0,0 +1,7 @@
+namespace LD58.World.Objects.WorldObjects
+{
+    [DefaultInstancer(64, "objects/Potted Plant.gmdl", "objects/Office.mat")]
+    public class PottedPlant
+        : WorldObject
+    { }
+}
