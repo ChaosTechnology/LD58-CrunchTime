dbabaee93f7b80f86653ac1d9afdb08ebbf27d6d
InsanityCode <56584762+InsanityCode@users.noreply.github.com>
2025-10-07 14:18:05 +0200

    add lunchbreak stub

--- LD58/source/World/Objectives/DoWork.cs
+++ LD58/source/World/Objectives/DoWork.cs
@@ -1,9 +1,43 @@
+using ChaosFramework.Components;
+using ChaosFramework.Math.Vectors;
+
 namespace LD58.World.Objectives
 {
+    using Interaction.Steps;
+    using Objects;
+    using Objects.WorldObjects;
+    using Player;
+
     class DoWork
         : Objective
     {
+        OfficeTable myDesk;
+
+        protected override void Create(CreateParameters cparams)
+        {
+            base.Create(cparams);
+            myDesk = scene.Find<OfficeTable>("MyDesk");
+            myDesk.AddComponent<Highlight>(CreateParameters.Create(myDesk.bone));
+        }
+
         protected override string GetText()
-            => "Sorry guys, we're running out of time,\nso maybe this is the end of it.\nEnjoy the beautiful office!";
+            => "Do work!";
+
+        public override bool Interact(Interactor interactor, Interactible interactible, Vector2i interactAt)
+        {
+            if (interactible == myDesk && myDesk.FacingScreenZero(interactor.parent.direction))
+            {
+                interactor.AddInteraction(
+                    new DialogLine(interactor, "Nah, let's skip this."),
+                    new CustomAction(interactor, Complete)
+                    );
+                return true;
+            }
+            else
+                return base.Interact(interactor, interactible, interactAt);
+        }
+
+        void Complete()
+            => scene.SetObjective<LunchBreak>();
     }
 }
