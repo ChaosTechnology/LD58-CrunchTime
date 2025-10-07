9b8d39ffac5c3dc50f8b8f9b45a80b141e0cd859
frameperfection <nafoizret@web.de>
2025-10-07 15:51:16 +0200

    fix keyFor

--- LD58/source/World/Objects/WorldObjects/PottedPlant.cs
+++ LD58/source/World/Objects/WorldObjects/PottedPlant.cs
@@ -3,5 +3,8 @@ namespace LD58.World.Objects.WorldObjects
     [DefaultInstancer(64, "objects/Potted Plant.gmdl", "objects/Office.mat")]
     public class PottedPlant
         : WorldObject
-    { }
+    {
+        [BoneParameter]
+        string keyFor;
+    }
 }
